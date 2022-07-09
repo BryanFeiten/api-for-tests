3cfb029567d6ff67171458831a2782ac1e9614ec
Bryan <bryan.feiten@growdev.com.br>
2022-07-09 13:46:48 -0300

    cria PostDto e método de extensão de Date para manipulação interna da aplicação

--- src/features/post/domain/dtos/post.dto.ts
+++ src/features/post/domain/dtos/post.dto.ts
@@ -1,6 +1,24 @@
-export type PostDto = {
-  uid?: string,
-  userUid: string,
-  title: string,
-  description: string,
-};
+import '../../../../shared/utils/extension_methods';
+
+
+export class PostDto {
+  public uid?: string;
+  public userUid: string;
+  public title: string;
+  public description: string;
+  public createdAt?: string;
+
+  constructor(
+    userUid: string,
+    title: string,
+    description: string,
+    uid?: string,
+  ) {
+    
+    this.uid = uid;
+    this.userUid = userUid;
+    this.title = title;
+    this.description = description;
+    this.createdAt = new Date().toAppDate();
+  };
+}
--- src/shared/utils/extension_methods.ts
+++ src/shared/utils/extension_methods.ts
@@ -1,8 +1,25 @@
 declare global {
+  interface Date {
+    toAppDate(): string;
+  }
+
   interface String {
     isEmpty(): boolean;
     isNotEmpty(): boolean;
   }
+}
+
+Date.prototype.toAppDate = function(this): string {
+  const seconds = this.getUTCSeconds() < 10 ? '0' + this.getUTCSeconds() : this.getUTCSeconds();
+  const minutes = this.getUTCMinutes() < 10 ? '0' + this.getUTCMinutes() : this.getUTCMinutes();
+  const hour = this.getUTCHours() < 10 ? '0' + this.getUTCHours() : this.getUTCHours();
+  const day = this.getUTCDate() < 10 ? '0' + this.getUTCDate() : this.getUTCDate();
+  const month = this.getUTCMonth() < 10 ? '0' + this.getUTCMonth() : this.getUTCMonth();
+  const year = this.getUTCFullYear() < 10 ? '0' + this.getUTCFullYear() : this.getUTCFullYear();
+
+  const dateFormat = `${year}/${month}/${day} ${hour}:${minutes}:${seconds}`;
+
+  return dateFormat;
 }
 
 String.prototype.isEmpty = function(this): boolean {
