7cbdd67ffb78c97f8e21dc252c385f469ce9a6f3
Bryan <bryan.feiten@growdev.com.br>
2022-07-15 18:01:02 -0300

    cria controller para deletar uma conta

---
+++ src/features/account/presentation/controllers/delete_account.controller.ts
@@ -0,0 +1,30 @@
+import {
+    Request,
+    Response
+} from "express";
+
+import { DeleteAccountUseCase } from "../../domain/usecases/delete_account.usecase";
+
+export class DeleteAccountController {
+    async handle(request: Request, response: Response) {
+        try {
+            const { accountUid } = request.params;
+            const password = request.body.password;
+
+            const useCase = new DeleteAccountUseCase();
+
+            await useCase.run(accountUid, password);
+
+            return response.status(200).send({
+                success: true,
+                data: true,
+                statusCode: 200,
+            });
+        } catch (error) {
+            return response.status(500).send({
+                success: false,
+                data: error instanceof Error ? error.message : "unknown",
+            });
+        }
+    }
+}
