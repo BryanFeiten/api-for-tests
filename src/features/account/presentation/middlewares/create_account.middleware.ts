ab7cdaa5c9a543f820f08628b7740c4fcb3a2022
Bryan <bryan.feiten@growdev.com.br>
2022-07-15 18:02:05 -0300

    cria middleware para validar os campos necessário para criar uma conta

---
+++ src/features/account/presentation/middlewares/create_account.middleware.ts
@@ -0,0 +1,43 @@
+import {
+  NextFunction,
+  Request,
+  Response,
+} from "express";
+
+import '../../../../shared/utils/extension_methods';
+
+export function createAccountMiddleware(
+  request: Request,
+  response: Response,
+  next: NextFunction,
+) {
+  const {
+    username,
+    firstName,
+    lastName,
+    email,
+    password,
+  } = request.body;
+
+  if ((username as string).trim().isEmpty()) {
+    throw new Error('Campo Nome de Usuário não foi preenchido');
+  };
+
+  if ((firstName as string).trim().isEmpty()) {
+    throw new Error('Campo Nome não foi preenchido');
+  };
+
+  if ((lastName as string).trim().isEmpty()) {
+    throw new Error('Campo Sobrenome não foi preenchido');
+  };
+
+  if ((email as string).trim().isEmpty()) {
+    throw new Error('Campo E-mail não foi preenchido');
+  };
+
+  if ((password as string).trim().isEmpty()) {
+    throw new Error('Campo Senha não foi preenchido');
+  };
+
+  next();
+};
