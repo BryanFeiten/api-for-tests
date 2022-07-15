c3fb77f40131dce557a7aada29f333243f03d6f5
Bryan <bryan.feiten@growdev.com.br>
2022-07-15 18:05:43 -0300

    cria caso de uso para atualizar título ou conteúdo de uma postagem

---
+++ src/features/post/domain/usecases/update_post.usecase.ts
@@ -0,0 +1,32 @@
+import '../../../../shared/utils/extension_methods';
+import { PostEntity } from "../../../../core/infra/database/entities/post";
+import { charactersLengthValidator } from "../../../../shared/utils/validators";
+import { PostRepository } from "../../infra/database/repositories/post.repository";
+import { PostDto } from "../dtos/post.dto";
+
+export class UpdatePostUseCase {
+    async run(post: PostDto): Promise<PostEntity> {
+        const repository = new PostRepository();
+
+        let actualPost: PostEntity;
+        let postUpdated: PostEntity;
+
+        if (post.title.isNotEmpty()) {
+            charactersLengthValidator(post.title.trim(), 'Título', 3, 50);
+        }
+
+        if (post.description.isNotEmpty()) {
+            charactersLengthValidator(post.description.trim(), 'Descrição', 2);
+        }
+
+        try {
+            actualPost = await repository.getByUid(post.uid!);
+
+            postUpdated = await repository.update(post, actualPost);
+        } catch (error) {
+            throw new Error('Erro na comunicação com o banco');
+        }
+
+        return postUpdated;
+    }
+}
