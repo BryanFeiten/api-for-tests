a887942ab6dd8dc5a363033f98176656265294d0
Bryan <bryan.feiten@growdev.com.br>
2022-07-15 18:00:33 -0300

    cria controller para listar todas as contas

---
+++ src/features/account/presentation/controllers/account_list.controller.ts
@@ -0,0 +1,26 @@
+import {
+    Request,
+    Response
+} from "express";
+
+import { GetAccountListUseCase } from "../../domain/usecases/get_account_list.usecase";
+
+export class AccountListController {
+    async handle(request: Request, response: Response) {
+        try {
+            const useCase = new GetAccountListUseCase();
+            const result = await useCase.run();
+
+            return response.status(200).send({
+                success: true,
+                data: result,
+                statusCode: 200,
+            });
+        } catch (error) {
+            return response.status(500).send({
+                success: false,
+                data: error instanceof Error ? error.message : "unknown",
+            });
+        }
+    }
+}
