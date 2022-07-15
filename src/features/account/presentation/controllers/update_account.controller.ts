32ebfce29201fc90b3752aa671a6d9473c2d04b3
Bryan <bryan.feiten@growdev.com.br>
2022-07-15 18:01:30 -0300

    cria controller para atualizar dados de uma conta

---
+++ src/features/account/presentation/controllers/update_account.controller.ts
@@ -0,0 +1,32 @@
+import {
+    Request,
+    Response
+} from "express";
+
+import { UpdateAccountUseCase } from "../../domain/usecases/update_account.usecase";
+import { AccountDto } from "../../domain/dtos/account.dto";
+
+export class UpdateAccountController {
+    async handle(request: Request, response: Response) {
+        const { accountUid, firstName, lastName } = request.body;
+
+        try {
+            const useCase = new UpdateAccountUseCase();
+            await useCase.run(
+                new AccountDto(accountUid, firstName.trim() ?? '', lastName.trim() ?? '', '', ''),
+            );
+
+            return response.status(200).send({
+                ok: true,
+                data: true,
+            });
+        } catch (error) {
+            return response.status(500).send({
+                ok: false,
+                data: error instanceof Error
+                    ? error.message
+                    : "unknown",
+            });
+        }
+    }
+}
