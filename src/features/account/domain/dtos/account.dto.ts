d7613ef45a440a5444708228d0f4fe2bfecc24c7
Bryan <bryan.feiten@growdev.com.br>
2022-07-15 17:55:58 -0300

    cria dto da conta e post

--- src/features/account/domain/dtos/account.dto.ts
+++ src/features/account/domain/dtos/account.dto.ts
@@ -1,4 +1,4 @@
-export class UserDto {
+export class AccountDto {
   public uid?: string;
   public username: string;
   public firstName: string;
--- src/features/post/domain/dtos/post.dto.ts
+++ src/features/post/domain/dtos/post.dto.ts
@@ -1,22 +1,21 @@
 import '../../../../shared/utils/extension_methods';
 
-
 export class PostDto {
   public uid?: string;
-  public userUid: string;
+  public accountUid: string;
   public title: string;
   public description: string;
   public createdAt?: string;
 
   constructor(
-    userUid: string,
+    accountUid: string,
     title: string,
     description: string,
     uid?: string,
   ) {
     
     this.uid = uid;
-    this.userUid = userUid;
+    this.accountUid = accountUid;
     this.title = title;
     this.description = description;
     this.createdAt = new Date().toAppDate();
