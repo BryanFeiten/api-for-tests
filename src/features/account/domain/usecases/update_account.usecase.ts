2e52932acc0895f4454ed798d0e0853d605e652a
Bryan <bryan.feiten@growdev.com.br>
2022-07-15 17:58:55 -0300

    cria usecase para atualizar nome e sobrenome (account)

---
+++ src/features/account/domain/usecases/update_account.usecase.ts
@@ -0,0 +1,39 @@
+import '../../../../shared/utils/extension_methods';
+import { AccountEntity } from "../../../../core/infra/database/entities/account";
+import { CacheRepository } from "../../../../core/infra/database/repositories/cache.repository";
+import { charactersLengthValidator } from "../../../../shared/utils/validators";
+import { AccountRepository } from "../../infra/database/repositories/account.repository";
+import { AccountDto } from "../dtos/account.dto";
+
+export class UpdateAccountUseCase {
+    async run(account: AccountDto): Promise<boolean> {
+        // const cacheRepository = new CacheRepository();
+        const repository = new AccountRepository();
+        let actualAccount: AccountEntity;
+        let accountUpdated: boolean;
+
+        if (account.firstName.isNotEmpty()) {
+            charactersLengthValidator(account.firstName.trim(), 'Nome', 3, 30);
+        }
+
+        if (account.lastName.isNotEmpty()) {
+            charactersLengthValidator(account.lastName.trim(), 'Sobrenome', 3, 30);
+        }
+
+        try {
+            actualAccount = await repository.getByUid(account.uid!);
+
+            accountUpdated = await repository.update(account, actualAccount);
+        } catch (error) {
+            throw new Error('Erro na comunicação com o banco');
+        }
+
+        // cache geral
+        // await cacheRepository.delete("users");
+
+        // cache do user
+        // await cacheRepository.delete(`users:${params.username}`);
+        // await cacheRepository.setEx(`users:${params.username}`, user);
+        return accountUpdated;
+    }
+}
