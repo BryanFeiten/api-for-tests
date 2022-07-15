4ca8acc5f0dc92c5ecd647af88caa496b11dec5d
Bryan <bryan.feiten@growdev.com.br>
2022-07-15 18:01:17 -0300

    cria controller para buscar uma conta específica

---
+++ src/features/account/presentation/controllers/get_account.controller.ts
@@ -0,0 +1,28 @@
+import {
+    Request,
+    Response
+} from "express";
+
+import { GetAccountUseCase } from "../../domain/usecases/get_account.usecase";
+
+export class GetAccountController {
+    async handle(request: Request, response: Response) {
+        const { username } = request.params;
+        const useCase = new GetAccountUseCase();
+
+        try {
+            const result = await useCase.run(username);
+
+            return response.status(200).send({
+                success: true,
+                data: result,
+                statusCode: 200,
+            });
+        } catch (error) {
+            return response.status(500).send({
+                success: false,
+                data: error instanceof Error ? error.message : "unknown",
+            });
+        }
+    }
+}
