aa05f6ee42901c5b207fa700176879335799af45
Bryan <bryan.feiten@growdev.com.br>
2022-07-18 20:21:36 -0300

    adiciona middlewares nas suas respectivas rotas

--- src/features/account/presentation/routes/account.routes.ts
+++ src/features/account/presentation/routes/account.routes.ts
@@ -9,29 +9,33 @@ import { DeleteAccountController } from '../controllers/delete_account.controlle
 import { CreateAccountController } from '../controllers/create_account.controller';
 import { GetAccountController } from '../controllers/get_account.controller';
 import { AccountListController } from '../controllers/account_list.controller';
-import { createAccountMiddleware } from '../middlewares/create_account.middleware';
+import { CreateAccountMiddleware } from '../middlewares/create_account.middleware';
+import { UpdateAccountMiddleware } from '../middlewares/update_account.middleware';
+import { DeleteAccountMiddleware } from '../middlewares/delete_account.middleware';
+import { GetAccountByUsernameMiddleware } from '../middlewares/get_account_by_username.middleware';
+import { AuthMiddleware } from '../../../../shared/presentation/middlewares/authentication.middleware';
 
 export class AccountRoutes {
     static getRoutes() {
         const router = Router();
 
-        router.post('/', createAccountMiddleware, (request: Request, response: Response) => {
+        router.post('/', CreateAccountMiddleware, (request: Request, response: Response) => {
             return new CreateAccountController().handle(request, response);
         });
 
-        router.get('/:username', (request: Request, response: Response) => {
+        router.get('/:username', [AuthMiddleware, GetAccountByUsernameMiddleware], (request: Request, response: Response) => {
             return new GetAccountController().handle(request, response);
         });
 
-        router.get('/', (request: Request, response: Response) => {
+        router.get('/', AuthMiddleware, (request: Request, response: Response) => {
             return new AccountListController().handle(request, response);
         });
 
-        router.delete('/:accountUid', (request: Request, response: Response) => {
+        router.delete('/', [AuthMiddleware, DeleteAccountMiddleware], (request: Request, response: Response) => {
             return new DeleteAccountController().handle(request, response);
         });
 
-        router.put('/', (request: Request, response: Response) => {
+        router.put('/', [AuthMiddleware, UpdateAccountMiddleware], (request: Request, response: Response) => {
             return new UpdateAccountController().handle(request, response);
         });
 
