15bc54312840a2bc1bfe1b51e59342191125d3c0
Bryan <bryan.feiten@growdev.com.br>
2022-07-19 21:24:31 -0300

    reconfigura ormConfig de acordo com o ambiente

--- ormconfig.js
+++ ormconfig.js
@@ -1,14 +1,56 @@
-const env = process.env.NODE_ENV === "production" ? "dist" : "src";
 
-module.exports = {
-    type: "sqlite",
-    database: "db.sqlite3",
-    entities: [`${env}/core/infra/database/entities/*.ts`],
-    migrations: [`${env}/core/infra/database/migrations/*.ts`],
-    logging: false,
-    synchronize: false,
-    cli: {
-        entitiesDir: "src/core/infra/database/entities",
-        migrationsDir: "src/core/infra/database/migrations",
-    },
-};
+require('dotenv').config();
+
+let config = {};
+
+const enviroment = process.env.NODE_ENV.toLowerCase();
+
+switch (enviroment) {
+    case 'production':
+        config = {
+            type: process.env.DATABASE_TYPE,
+            url: process.env.DATABASE_URI,
+            logging: false,
+            extra: {
+                ssl: {
+                    rejectUnauthorized: false
+                }
+            },
+            entities: ['dist/core/infra/database/entities/*.ts'],
+            migrations: ['dist/core/infra/database/migrations/*.ts'],
+            cli: {
+                entitiesDir: "dist/core/infra/database/entities",
+                migrationsDir: "dist/core/infra/database/migrations",
+            },
+        };
+        break;
+
+    case 'test':
+        config = {
+            type: 'sqlite',
+            database: './test-database.sql',
+            migrations: ['src/database/migrations/**/*'],
+            entities: ['src/database/entities/**/*']
+        };
+        break;
+
+    default:
+        config = {
+            type: process.env.DATABASE_TYPE,
+            url: process.env.DATABASE_URI,
+            logging: false,
+            extra: {
+                ssl: {
+                    rejectUnauthorized: false
+                }
+            },
+            entities: ['src/core/infra/database/entities/*.ts'],
+            migrations: ['src/core/infra/database/migrations/*.ts'],
+            cli: {
+                entitiesDir: "src/core/infra/database/entities",
+                migrationsDir: "src/core/infra/database/migrations",
+            },
+        };
+}
+
+module.exports = config;
